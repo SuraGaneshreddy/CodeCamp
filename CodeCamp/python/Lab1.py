8e41ef72e7395dbc3510c693ebb8583bf3488287
Sura Ganesh Reddy <sura.ganeshreddy060906@gmail.com>
2026-02-24 21:54:04 +0530

    Add files via upload

---
+++ CodeCamp/python/Lab1.py
@@ -0,0 +1,28 @@
+# Variables
+distance_mi = 3
+is_raining = False
+has_bike = True
+has_car = False
+has_ride_share_app = False
+
+# Conditional logic
+if not distance_mi:
+    print(False)
+
+elif distance_mi <= 1:
+    if not is_raining:
+        print(True)
+    else:
+        print(False)
+
+elif distance_mi <= 6:
+    if has_bike and not is_raining:
+        print(True)
+    else:
+        print(False)
+
+else:  # distance_mi > 6
+    if has_car or has_ride_share_app:
+        print(True)
+    else:
+        print(False)
